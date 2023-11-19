a070ff4fb12a5c204d2480c6466387d63daf48cf
tdd <141925115+zx9999r@users.noreply.github.com>
2023-11-20 00:05:52 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,47 @@
+import os
+pip install discord
+pip install ping3
+pip install aiohttp
+pip install asyncio
+pip install random
+pip install requests
+pip install psutil
+pip install threading
+pip install bs4
+pip install speedtest-cli
+pip install PySocks
+pip install cfscrape
+pip install datetime
+pip install ssl
+pip install imcplib
+pip install scapy
+npm install mongoose
+npm install keyv
+npm install dotenv
+npm install @keyv/sqlite
+npm install cpu-stat
+npm install chalk
+npm install fake-useragent
+npm install request
+npm install request
+npm install moment-duration-format
+npm i cloudflare-bypasser
+npm i cloudscraper
+sudo apt-get install nodejs
+sudo apt-get install npm
+npm i events
+npm i fs
+npm i url
+npm i net
+npm i cloudscraper
+npm i request
+npm i randomstring
+npm i cluster
+npm i hcaptcha-solver
+npm install user-agents
+npm install header-generator
+npm install moment
+npm install https
+npm install path
+npm install puppeteer
+npm install axios
